1dcdf8ea356c36a545e568efcee44efca1a30e86
FM-TI <FM-TI@192.168.15.84>
2023-06-27 11:52:16 -0300

    27-06

---
+++ cursosAlura/src/poo/CriarConta.java
@@ -0,0 +1,22 @@
+package poo;
+
+public class CriarConta {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Conta primeiraConta = new Conta();
+		
+		primeiraConta.saldo = 200;
+		
+		System.out.println(primeiraConta.saldo);
+		
+		
+		Conta segundaConta = new Conta();
+		segundaConta.saldo = 50;
+		
+		System.out.println(segundaConta.saldo);
+		
+	}
+
+}
